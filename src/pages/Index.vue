a67024a3950fcedcb519935e0b146835656d68f2
Paul Hwang <mothcar@gmail.com>
2020-09-05 09:04:31 +0900

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="">
+      <h1 >title</h1>
+      <h6 id="bigOne"></h6>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  methods: {
+
+  },
+
+  mounted() {
+    var bigOne = document.getElementById('bigOne')
+    var dbRef = firebase.database().ref().child('text')
+    dbRef.on('value', snap => bigOne.innerText = snap.val())
+    console.log('Ref : ', dbRef)
+  },
+}
+</script>
