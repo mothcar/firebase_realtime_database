7c06f608a3bf787388e8535c7ce5d69e4bde157f
Paul Hwang <mothcar@gmail.com>
2020-09-05 10:19:32 +0900

    create column

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column">
     <div class="">
-      <h1 >title</h1>
       <h6 id="bigOne"></h6>
+    </div>
+
+    <div class="">
+      <q-form
+        @submit="onSubmit"
+        @reset="onReset"
+        class="q-gutter-md"
+      >
+        <q-input
+          filled
+          v-model="name"
+          label="Your name *"
+          hint="Name and surname"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Please type something']"
+        />
+
+        <q-input
+          filled
+          type="number"
+          v-model="age"
+          label="Your age *"
+          lazy-rules
+          :rules="[
+            val => val !== null && val !== '' || 'Please type your age',
+            val => val > 0 && val < 100 || 'Please type a real age'
+          ]"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+          <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </q-form>
+
     </div>
   </q-page>
 </template>
@@ -11,9 +45,38 @@
 export default {
   name: 'PageIndex',
 
-  methods: {
+  data () {
+    return {
+      name: null,
+      age: null,
 
+    }
   },
+
+
+  methods: {
+    onSubmit () {
+      let database = firebase.database()
+      let rootRef = database.ref('users')
+      var autoId = rootRef.push().key
+      
+      var name = this.name
+      var age = this.age
+      // database.ref('/users'+ '0').set({
+      //   name: name,
+      //   age: age
+      // })
+      rootRef.child(autoId).set({
+        name: name,
+        age: age
+      })
+    },
+
+    onReset () {
+      this.name = null
+      this.age = null
+    }
+  }, //methods
 
   mounted() {
     var bigOne = document.getElementById('bigOne')
